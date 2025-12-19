0490995666d2d67e8cc35e2e4e54da0cbc263f05
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2025-12-19 16:50:31 +1300

    remove toggle

--- frontend/components/RootClientLayout.tsx
+++ frontend/components/RootClientLayout.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react'
 import Footer from './Footer'
 import Navbar from './Navbar'
 import TopLogos from './TopLogos'
-import Toggle from './Toggle'
+// import Toggle from './Toggle'
 import PageWrapper from './PageWrapper'
 import MobileMenuToggle from './MobileMenuToggle'
 
@@ -39,14 +39,12 @@ export default function RootClientLayout({
 
       <div className=" sticky flex items-center justify-between lg:hidden px-6 top-0 z-50 bg-gradient-to-r from-[#8A5082] to-[#A5CAD2] py-1 mt-4 shadow-[0_2px_10px_rgba(0,0,0,0.35)]">
         <MobileMenuToggle />
-        <Toggle />
+        {/* <Toggle /> */}
       </div>
 
       {/* Sticky Toggle */}
       <div className="sticky top-0 z-50 hidden lg:block  px-0">
-        <div className="">
-          <Toggle />
-        </div>
+        <div className="">{/* <Toggle /> */}</div>
       </div>
 
       {/* Sticky Navbar under Toggle */}
